1296da4804641f38825ca80c591c20f2ed149cdf
Priyanka <priyanka.msharma@gmail.com>
2022-06-07 12:03:56 -0400

    Changes in front end

--- PetzWorld/ViewModels/AddEventViewModel.cs
+++ PetzWorld/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PetzWorld.Models;
 
 namespace PetzWorld.ViewModels
 {
-    public class AddEventViewModel : Controller
+    public class AddEventViewModel
     {
         [Required(ErrorMessage = "Event Name is required!")]
         public string Name { get; set; }
